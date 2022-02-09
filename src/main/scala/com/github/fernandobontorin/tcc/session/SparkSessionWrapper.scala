6be2bf969017953f9951b1f89e4df47276dc6a0f
Fernando Bontorin <fernandorbontorin@gmail.com>
2022-02-08 21:13:54 -0300

    add ci clear repo

--- src/main/scala/com/github/fernandobontorin/tcc/session/SparkSessionWrapper.scala
+++ src/main/scala/com/github/fernandobontorin/tcc/session/SparkSessionWrapper.scala
@@ -1,11 +1,11 @@
-package com.github.fernandobontorin.tcc
+package com.github.fernandobontorin.tcc.session
 
 import org.apache.spark.sql.SparkSession
 
 trait SparkSessionWrapper extends Serializable {
 
   lazy val spark: SparkSession = {
-    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+    SparkSession.builder().getOrCreate()
   }
 
 }
